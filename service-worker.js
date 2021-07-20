644324875cb899884d87c4f86d5283e2002dc47c
ruben <ruben.martin@academiadeinventores.com>
2021-07-20 15:53:00 +0200

    Test plugins

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.bf736cf8507e70346b247220489e86f3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.d4861e0f121a017cf1b302ee005061a9.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // service-worker.js
 
