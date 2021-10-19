aca72a3b3d04b6e3f68d3b412bd2fbf43a3e4811
ruben <ruben.martin@academiadeinventores.com>
2021-10-19 23:36:26 +0200

    [wip]  screenshot with touch devices

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.c36c301b5cf2b2e74b3434c209966efb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.be6aff3786ed1f47be3ad64883d135ff.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // service-worker.js
 
