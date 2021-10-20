47514f735d36f5355150c9d786532941a1c086b8
ruben <ruben.martin@academiadeinventores.com>
2021-10-20 14:17:06 +0200

    [fix] screenshot touch

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.8835feb836f8a9b6a2e1a4340083f532.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.62c39c97c45aefe18e99318de1f1cf1e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // service-worker.js
 
